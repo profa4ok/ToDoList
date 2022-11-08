cdf86a01bd6172610ec16ede974abc285c9821ff
Radyush_Maksim <profa4oka98@mail.ru>
2022-11-08 13:10:30 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Максим Радюш on 8.11.22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
